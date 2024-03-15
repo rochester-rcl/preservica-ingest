c1f61f4fc56545c663d246dfe2d5eb1df178e3a3
johnd-rcl <98056458+johnd-rcl@users.noreply.github.com>
2024-03-15 17:43:48 -0400

    Update cleanup_dates.py
    fixed an error where there could be identical starting and ending dates in a range

--- cleanup_dates.py
+++ cleanup_dates.py
@@ -1,12 +1,11 @@
 import re
 
-
 def aspace_dates(aspace_date):
     date_undated = re.findall('undated', aspace_date)
-    date_ymd = re.findall('^creation:\s(\d\d\d\d-\d\d-\d\d)$', aspace_date)
-    date_ym = re.findall('^creation:\s(\d\d\d\d-\d\d)$', aspace_date)
-    date_y = re.findall('^creation:\s(\d\d\d\d)$', aspace_date)
-    date_range = re.findall('creation:\s(.+?--.+)', aspace_date)
+    date_ymd = re.findall('^creation: (\d\d\d\d-\d\d-\d\d)$', aspace_date)
+    date_ym = re.findall('^creation: (\d\d\d\d-\d\d)$', aspace_date)
+    date_y = re.findall('^creation: (\d\d\d\d)$', aspace_date)
+    date_range = re.findall('creation: (.+?--.+)', aspace_date)
     date_multiple = re.findall('^creation:\s.+?;\screation:\s.+?$', aspace_date)
 
     def month_convert(month):
@@ -34,7 +33,7 @@ def aspace_dates(aspace_date):
             return 'November'
         elif month == '12':
             return 'December'
-
+        
     def ymd_convert(convert_ymd):
         date_year = convert_ymd[0].split('-')[0].strip()
         iso_month = convert_ymd[0].split('-')[1].strip()
@@ -42,19 +41,19 @@ def aspace_dates(aspace_date):
         date_day = int(convert_ymd[0].split('-')[2].strip())
         ymd_converted = '{date_month} {date_day}, {date_year}'.format(date_month = date_month, date_day = date_day, date_year = date_year)
         return ymd_converted
-
+    
     def ym_convert(convert_ym):
         date_year = convert_ym[0].split('-')[0].strip()
         iso_month = convert_ym[0].split('-')[1].strip()
         date_month = month_convert(iso_month)
         ym_converted = '{date_month} {date_year}'.format(date_month = date_month, date_year = date_year)
         return ym_converted
-
+    
     def y_convert(convert_y):
         date_year = convert_y[0].strip()
         y_converted = '{date_year}'.format(date_year = date_year)
         return y_converted
-
+    
     if len(date_undated) == 1:
         date_formatted = ' (undated)'
         return date_formatted
@@ -68,28 +67,28 @@ def aspace_dates(aspace_date):
         date_formatted = ym_convert(date_ym)
         date_formatted = ', ' + date_formatted
         return date_formatted
-
+    
     if len(date_y) == 1:
         date_formatted = y_convert(date_y)
         date_formatted = ', ' + date_formatted
         return date_formatted
-
+    
     if len(date_range) == 1:
         date_first = date_range[0].split('--')[0].strip()
         date_first_ymd = re.findall('(\d\d\d\d-\d\d-\d\d)$', date_first)
-        date_first_ym = re.findall('(\d\d\d\d-\d\d)$', date_first)
-        date_first_y = re.findall('(\d\d\d\d)$', date_first)
+        date_first_ym = re.findall('\d\d\d\d-\d\d$', date_first)
+        date_first_y = re.findall('\d\d\d\d$', date_first)
         date_second = date_range[0].split('--')[1].strip()
         date_second_ymd = re.findall('(\d\d\d\d-\d\d-\d\d)$', date_second)
-        date_second_ym = re.findall('(\d\d\d\d-\d\d)$', date_second)
-        date_second_y = re.findall('(\d\d\d\d)$', date_second)
+        date_second_ym = re.findall('\d\d\d\d-\d\d$', date_second)
+        date_second_y = re.findall('\d\d\d\d$', date_second)
 
         if len(date_first_ymd) == 1:
             date_first_formatted = ymd_convert(date_first_ymd)
 
         if len(date_first_ym) == 1:
             date_first_formatted = ym_convert(date_first_ym)
-
+        
         if len(date_first_y) == 1:
             date_first_formatted = y_convert(date_first_y)
 
@@ -98,29 +97,33 @@ def aspace_dates(aspace_date):
 
         if len(date_second_ym) == 1:
             date_second_formatted = ym_convert(date_second_ym)
-
+        
         if len(date_second_y) == 1:
             date_second_formatted = y_convert(date_second_y)
 
-        date_formatted = ', {date_first_formatted} - {date_second_formatted}'.format(date_first_formatted=date_first_formatted, date_second_formatted=date_second_formatted)
+        if date_first_formatted == date_second_formatted:
+            date_formatted = ', {date_first_formatted}'.format(date_first_formatted=date_first_formatted)
+        else:
+            date_formatted = ', {date_first_formatted} - {date_second_formatted}'.format(date_first_formatted=date_first_formatted, date_second_formatted=date_second_formatted)
+        
         return date_formatted
 
     if len(date_multiple) == 1:
         date_first = date_multiple[0].split(';')[0].split()[1].strip()
         date_first_ymd = re.findall('(\d\d\d\d-\d\d-\d\d)$', date_first)
-        date_first_ym = re.findall('(\d\d\d\d-\d\d)$', date_first)
-        date_first_y = re.findall('(\d\d\d\d)$', date_first)
+        date_first_ym = re.findall('\d\d\d\d-\d\d$', date_first)
+        date_first_y = re.findall('\d\d\d\d$', date_first)
         date_second = date_multiple[0].split(';')[1].split()[1].strip()
         date_second_ymd = re.findall('(\d\d\d\d-\d\d-\d\d)$', date_second)
-        date_second_ym = re.findall('(\d\d\d\d-\d\d)$', date_second)
-        date_second_y = re.findall('(\d\d\d\d)$', date_second)
+        date_second_ym = re.findall('\d\d\d\d-\d\d$', date_second)
+        date_second_y = re.findall('\d\d\d\d$', date_second)
 
         if len(date_first_ymd) == 1:
             date_first_formatted = ymd_convert(date_first_ymd)
 
         if len(date_first_ym) == 1:
             date_first_formatted = ym_convert(date_first_ym)
-
+        
         if len(date_first_y) == 1:
             date_first_formatted = y_convert(date_first_y)
 
@@ -129,7 +132,7 @@ def aspace_dates(aspace_date):
 
         if len(date_second_ym) == 1:
             date_second_formatted = ym_convert(date_second_ym)
-
+        
         if len(date_second_y) == 1:
             date_second_formatted = y_convert(date_second_y)
 
